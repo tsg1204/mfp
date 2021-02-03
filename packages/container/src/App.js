dc7f4829ea92a0d3db009c2810276993c1a1d450
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-03 13:09:51 -0500

    minor text changes in app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Testing</h1>
+      <h1>Making changes and testing!</h1>
       <hr />
       <MarketingApp />
     </div>
