e96467159ab6e4e8e8c922ca25b4bb55c3e329cc
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-03 00:02:54 -0500

    generic integration of marketing and container setup

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { mount } from 'marketing/MarketingApp';
-
-console.log(mount);
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
     <div>
       <h1>Hi there </h1>
+      <hr />
+      <MarketingApp />
     </div>
   );
 };
