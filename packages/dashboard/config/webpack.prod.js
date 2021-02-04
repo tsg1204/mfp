e0a84ccbaab278051d4f35f9b2e525e15fbbb03a
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-04 17:26:39 -0500

    dashboard webapack prod fixed

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
       name: 'dashboard',
       filename: 'remoteEntry.js',
       exposes: {
-        './Dashboard/App': './src/bootstrap',
+        './DashboardApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
